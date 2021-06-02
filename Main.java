1f717c8a5fa83ff6adc7233bb9dabcde5bb14f06
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-02 14:59:02 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import com.FirstPack.FirstOne;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FirstOne firstOne = new FirstOne();
+        firstOne.SomeFunction();//Error
+
+
+    }
+}
